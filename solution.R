d64b5f0fd1fb8a850413818a6adfb8f88c159159
Moutaz Ali <moutaz.ali.dev@gmail.com>
2025-04-15 19:32:53 +0200

    adding project files and first table of demogrpphic data

---
+++ solution.R
@@ -0,0 +1,29 @@
+rm(list = ls())
+graphics.off()
+
+library(tidyverse)
+library(psych)
+library(janitor)
+library(gtsummary)
+library(cardx)
+
+data<- read_csv("./data.csv") %>% 
+  clean_names(.)
+
+data <- data %>% 
+  mutate(service_in_years = as.numeric(service_in_years)) %>% 
+  replace_na(list(service_in_years = 0))
+
+
+data %>% 
+  gtsummary::tbl_summary()
+
+
+tb1 <- data %>% 
+  select(1:12) %>% 
+  tbl_summary() %>% 
+  bold_labels() %>% 
+  modify_caption("Demographic Data")
+
+
+tb1
